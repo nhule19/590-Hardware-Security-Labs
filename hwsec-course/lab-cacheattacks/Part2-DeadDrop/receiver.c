550c1264e37648abda0d5ed74fef995f97c8c735
Andre Alain Kudzo Rastrelly <arastrel@burnsville.cs.unc.edu>
2026-03-12 13:25:23 -0400

    8 bit channel

--- hwsec-course/lab-cacheattacks/Part2-DeadDrop/receiver.c
+++ hwsec-course/lab-cacheattacks/Part2-DeadDrop/receiver.c
@@ -10,61 +10,124 @@
 
 #define BUFF_SIZE (1<<21)
 
+// ! SINGLE BIT TRANSFER THAT WORKS
+
+// int main(int argc, char **argv)
+// {
+// 	// Put your covert channel setup code here
+// 	int fd = shm_open("/covert_channel", O_CREAT | O_RDWR, 0666);
+// 	char *buf = mmap(NULL, BUFF_SIZE,
+// 		PROT_READ|PROT_WRITE,
+// 		MAP_SHARED, fd, 0);
+  
+//     if (buf == (void*) - 1) {
+//      perror("mmap() error\n");
+//      exit(EXIT_FAILURE);
+//     }
+
+// 	printf("Please press enter.\n");
+
+// 	char text_buf[2];
+// 	fgets(text_buf, sizeof(text_buf), stdin);
+
+// 	// printf("Receiver now listening.\n");
+
+// 	bool listening = true;
+// 	int bit;
+// 	CYCLES latency;
+
+// 	while (listening) {
+// 		printf("Receiver now listening.\n");
+// 		// Put your covert channel code here
+// 		volatile char *flag = (volatile char *)&buf[2048];
+// 		// Wait for sender to signal
+// 		*flag = 0;
+// 		while (*flag != 1) {
+//     		// loop until signal received
+// 		}
+
+// 		// // Warm up TLB and page table for buf[0]'s page
+// 		// volatile char warmup = buf[64];
+// 		// (void)warmup;
+
+// 		// Measure latency
+// 		latency = measure_one_block_access_time((uint64_t)buf);
+
+// 		if (latency > 80) { // miss, L3 cache -->  bit is 0
+// 			bit = 0;
+// 		} else { // hit, L2 cache --> bit is 1
+// 			bit = 1;
+// 		}
+// 		listening = false;
+		
+// 	}
+
+// 	printf("\nBit: %d\n", bit);
+// 	printf("Latency: %d\n", (int)latency);
+// 	printf("Receiver finished.\n");
+
+// 	return 0;
+// }
+
+// ! END SINGLE BIT TRANSFER
+
 int main(int argc, char **argv)
 {
-	// Put your covert channel setup code here
-	int fd = shm_open("/covert_channel", O_CREAT | O_RDWR, 0666);
-	char *buf = mmap(NULL, BUFF_SIZE,
-		PROT_READ|PROT_WRITE,
-		MAP_SHARED, fd, 0);
+    int fd = shm_open("/covert_channel", O_CREAT | O_RDWR, 0666);
+    char *buf = mmap(NULL, BUFF_SIZE,
+        PROT_READ|PROT_WRITE,
+        MAP_SHARED, fd, 0);
   
     if (buf == (void*) - 1) {
-     perror("mmap() error\n");
-     exit(EXIT_FAILURE);
+        perror("mmap() error\n");
+        exit(EXIT_FAILURE);
     }
 
-	printf("Please press enter.\n");
+    printf("Please press enter.\n");
+    char text_buf[2];
+    fgets(text_buf, sizeof(text_buf), stdin);
 
-	char text_buf[2];
-	fgets(text_buf, sizeof(text_buf), stdin);
+    printf("Receiver now listening.\n");
 
-	// printf("Receiver now listening.\n");
+    volatile char *flag1 = (volatile char *)&buf[4096];
+    volatile char *flag2 = (volatile char *)&buf[8192];
+    *flag1 = 0;
+    *flag2 = 0;
 
-	bool listening = true;
-	int bit;
-	CYCLES latency;
+    unsigned int message = 0;
+    CYCLES latency;
+	int bits[8];
+	volatile CYCLES latencies[8];
 
-	while (listening) {
-		printf("Receiver now listening.\n");
-		// Put your covert channel code here
-		volatile char *flag = (volatile char *)&buf[4096];
-		// Wait for sender to signal
-		*flag = 0;
-		while (*flag != 1) {
-    		// loop until signal received
+	// bring buf[0]'s table into position
+	volatile char warmup = buf[0];
+	(void)warmup;
+
+    for (int bit_index = 7; bit_index >= 0; bit_index--) {
+        // wait for sender to signal it's ready
+        while (*flag1 != 1) { }
+
+        // measure latency of buf[0]
+        latencies[bit_index] = measure_one_block_access_time((uint64_t)buf);
+
+        int bit;
+		if (latencies[bit_index] > 130) {
+			bits[bit_index] = 0;
+		} else {
+			bits[bit_index] = 1;
 		}
-		// Warm up TLB and page table for buf[0]'s page
-		volatile char warmup = buf[64];
-		(void)warmup;
+        
+        message |= (bits[bit_index] << bit_index);
 
-		// Measure latency
-		latency = measure_one_block_access_time((uint64_t)buf);
-
-		if (latency > 150) { // miss, L3 cache -->  bit is 0
-			bit = 0;
-		} else { // hit, L2 cache --> bit is 1
-			bit = 1;
-		}
-		listening = false;
-		
+        // signal sender to proceed
+        *flag1 = 0;
+        *flag2 = 1;
+    }
+	for (int i = 7; i >= 0; i--) {
+		printf("\nBit %d: %d (latency: %d)\n", i, bits[i], (int)latencies[i]);
 	}
-
-	printf("\nBit: %d\n", bit);
-	printf("Latency: %d\n", (int)latency);
-	printf("Receiver finished.\n");
-
-	return 0;
+    printf("\nReceived message: %d\n", message);
+    printf("Receiver finished.\n");
+    return 0;
 }
-
-
 
--- hwsec-course/lab-cacheattacks/Part2-DeadDrop/sender.c
+++ hwsec-course/lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -15,82 +15,240 @@
 
 #define LINE_SIZE 64
 
-void delay (int seconds) {
-	long pause = seconds * CLOCKS_PER_SEC;
-	clock_t start = clock();
-	while (clock() - start < pause);
-}
+// int main(int argc, char **argv)
+// {
+
+//     int fd = shm_open("/covert_channel", O_CREAT | O_RDWR, 0666);
+//     ftruncate(fd, BUFF_SIZE);
+
+//     // Allocate a buffer using huge page
+//     // See the handout for details about hugepage management
+//     char *buf = mmap(NULL, BUFF_SIZE,
+//         PROT_READ|PROT_WRITE,
+//         MAP_SHARED, fd, 0);
+  
+//     if (buf == (void*) - 1) {
+//      perror("mmap() error\n");
+//      exit(EXIT_FAILURE);
+//     }
+
+    
+//     // The first access to a page triggers overhead associated with
+//     // page allocation, TLB insertion, etc.
+//     // Thus, we use a dummy write here to trigger page allocation
+//     // so later access will not suffer from such overhead.
+//     *((char *)buf) = 1; // dummy write to trigger page allocation
+
+//     volatile char tmp;
+
+//     // TODO:
+//     // Put your covert channel setup code here
+
+//     uint64_t *eviction_buffer = (uint64_t *)malloc(L1_SIZE + L2_SIZE + 100000);
+
+//     if (eviction_buffer == NULL) {
+//         perror("Unable to malloc eviction buffer");
+//         return EXIT_FAILURE;
+//     }
+
+//     // printf("Please type a message.\n");
+//     bool sending = true;
+//     while (sending) {
+//         printf("Please type a message.\n");
+//         char text_buf[128];
+//         fgets(text_buf, sizeof(text_buf), stdin);
+        
+//         // TODO:
+//         // Put your covert channel code here
+
+//         volatile char *flag = (volatile char *)&buf[4096];
+//         volatile char *flag2 = (volatile char *)&buf[8192];
+
+//         int i = 0;
+        
+//         while (text_buf[i] != '\n' && text_buf[i] != '\0') {
+    
+//             int number_value = text_buf[i] - '0';
+            
+//             for (volatile int bit_in_char = 0; bit_in_char < 4; bit_in_char++) {
+//                 *flag2 = 0;
+//                 // int transmit_bit = (number_value >> bit_in_char) & 0b1;
+//                 int volatile transmit_bit = (number_value >> (7 - bit_in_char)) & 1;
+
+//                 // printf("\nBit to transmit: %d\n", transmit_bit);
+
+//                 if (transmit_bit == 0b1) { // make latency small
+//                     for (volatile int j = 0; j < 1090; j++) {
+//                         tmp = ((char*)buf)[0];
+//                     }
+//                 } else { // make latency big (evict)
+//                     for (volatile int j = 0; j < (L1_SIZE + L2_SIZE + 100000) / 8; j++) {
+//                         tmp = eviction_buffer[j];
+//                     }
+//                 }
+//                 // signal to receiver that bit is ready
+                
+//                 *flag = 1;
+
+//                 for (volatile int dummy = 0; dummy < 100000; dummy++);
+
+//                 // printf("\nArrived at bit: %d\n", bit_in_char);
+//                 while (*flag2 != 1) {
+//                     // wait until receiver is ready to move on to bext bit
+//                 }
+//             }
+//             i++;
+//         }
+//         sending = false;
+//     }
+
+//   printf("Sender finished.\n\n\n");
+//   free(eviction_buffer);
+//   return 0;
+// }
+
+
+// ! SINGLE BIT TRANSFER THAT WORKS
+
+// int main(int argc, char **argv)
+// {
+
+//     int fd = shm_open("/covert_channel", O_CREAT | O_RDWR, 0666);
+//     ftruncate(fd, BUFF_SIZE);
+
+//     // Allocate a buffer using huge page
+//     // See the handout for details about hugepage management
+//     char *buf = mmap(NULL, BUFF_SIZE,
+//         PROT_READ|PROT_WRITE,
+//         MAP_SHARED, fd, 0);
+  
+//     if (buf == (void*) - 1) {
+//      perror("mmap() error\n");
+//      exit(EXIT_FAILURE);
+//     }
+
+    
+//     // The first access to a page triggers overhead associated with
+//     // page allocation, TLB insertion, etc.
+//     // Thus, we use a dummy write here to trigger page allocation
+//     // so later access will not suffer from such overhead.
+//     *((char *)buf) = 1; // dummy write to trigger page allocation
+
+//     volatile char tmp;
+
+//     // TODO:
+//     // Put your covert channel setup code here
+
+//     uint64_t *eviction_buffer = (uint64_t *)malloc(L3_SIZE * 2);
+
+//     if (eviction_buffer == NULL) {
+//         perror("Unable to malloc eviction buffer");
+//         return EXIT_FAILURE;
+//     }
+
+//     // printf("Please type a message.\n");
+//     bool sending = true;
+//     while (sending) {
+//         printf("Please type a message.\n");
+//         char text_buf[128];
+//         fgets(text_buf, sizeof(text_buf), stdin);
+
+//         // TODO:
+//         // Put your covert channel code here
+
+//         volatile char c = text_buf[0];
+//         volatile int transmit_bit = c & 0b1;
+
+//         printf("\nBit to transmit: %d\n", transmit_bit);
+
+//         *((char *)buf) = 1;
+
+//         if (transmit_bit == 0b1) { // make latency small
+//             for (volatile int i = 0; i < 1090; i++) {
+//                 tmp = ((char*)buf)[0];
+//             }
+//         } else { // make latency big (evict)
+//             for (volatile int i = (L3_SIZE * 2) / sizeof(uint64_t); i >= 0 ; i--) {
+//                 tmp = eviction_buffer[i];
+//             }
+//         }
+//         sending = false;
+//         // Signal the receiver that the bit is ready
+//         volatile char *flag = (volatile char *)&buf[2048];
+//         *flag = 1;
+//         usleep(10);
+//         // for (volatile int i = 0; i < 100000; i++);
+//     }
+
+//   printf("Sender finished.\n\n\n");
+//   free(eviction_buffer);
+//   return 0;
+// }
+
+// ! END SINGLE BIT TRANSFER
 
 int main(int argc, char **argv)
 {
-
     int fd = shm_open("/covert_channel", O_CREAT | O_RDWR, 0666);
     ftruncate(fd, BUFF_SIZE);
 
-    // Allocate a buffer using huge page
-    // See the handout for details about hugepage management
     char *buf = mmap(NULL, BUFF_SIZE,
         PROT_READ|PROT_WRITE,
         MAP_SHARED, fd, 0);
   
     if (buf == (void*) - 1) {
-     perror("mmap() error\n");
-     exit(EXIT_FAILURE);
+        perror("mmap() error\n");
+        exit(EXIT_FAILURE);
     }
 
-    
-    // The first access to a page triggers overhead associated with
-    // page allocation, TLB insertion, etc.
-    // Thus, we use a dummy write here to trigger page allocation
-    // so later access will not suffer from such overhead.
     *((char *)buf) = 1; // dummy write to trigger page allocation
 
     volatile char tmp;
 
-    // TODO:
-    // Put your covert channel setup code here
-
-    uint64_t *eviction_buffer = (uint64_t *)malloc(L1_SIZE + L2_SIZE + 100000);
+    uint64_t *eviction_buffer = (uint64_t *)malloc(L3_SIZE * 2);
 
     if (eviction_buffer == NULL) {
         perror("Unable to malloc eviction buffer");
         return EXIT_FAILURE;
     }
 
-    // printf("Please type a message.\n");
-    bool sending = true;
-    while (sending) {
-        printf("Please type a message.\n");
-        char text_buf[128];
-        fgets(text_buf, sizeof(text_buf), stdin);
+    volatile char *flag1 = (volatile char *)&buf[4096];
+    volatile char *flag2 = (volatile char *)&buf[8192];
 
-        // TODO:
-        // Put your covert channel code here
+    printf("Please type a message.\n");
+    char text_buf[128];
+    fgets(text_buf, sizeof(text_buf), stdin);
 
-        char c = text_buf[0];
-        int transmit_bit = c & 0b1;
+    int value = string_to_int(text_buf);
 
-        printf("\nBit to transmit: %d\n", transmit_bit);
+    for (int bit_idx = 7; bit_idx >= 0; bit_idx--) {
+        int transmit_bit = (value >> bit_idx) & 1;
 
-        if (transmit_bit == 0b1) { // make latency small
-            for (int i = 0; i < 1090; i++) {
+        // Ensure buf[0] starts in cache before potential eviction
+        *((char *)buf) = 1;
+
+        if (transmit_bit == 1) { // keep in cache
+            for (volatile int i = 0; i < 109000; i++) {
                 tmp = ((char*)buf)[0];
             }
-        } else { // make latency big (evict)
-            for (int i = 0; i < (L1_SIZE + L2_SIZE + 100000) / 8; i++) {
-                tmp = eviction_buffer[i];
+        } else { // evict from cache
+            // Multiple passes over 2x L2 to defeat adaptive replacement
+            for (volatile int pass = 0; pass < 3; pass++) {
+                for (volatile size_t i = 0; i < (L3_SIZE * 2) / sizeof(uint64_t); i++) {
+                    tmp = eviction_buffer[i];
+                }
             }
         }
-        sending = false;
-        // Signal the receiver that the bit is ready
-        volatile char *flag = (volatile char *)&buf[4096];
-        *flag = 1;
-        for (volatile int i = 0; i < 100000; i++);
+
+        // signal receiver, yield so it can measure without cache interference
+        *flag1 = 1;
+        usleep(10);
+
+        // wait for receiver to finish measuring
+        while (*flag2 != 1) { }
+        *flag2 = 0;
     }
-
-  printf("Sender finished.\n\n\n");
-  free(eviction_buffer);
-  return 0;
+    printf("Sender finished.\n");
+    free(eviction_buffer);
+    return 0;
 }
-
-
